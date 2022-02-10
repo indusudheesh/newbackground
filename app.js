b4e15c55dd1b4933b88f8f8655adccf91354da9d
indusudheesh <93575341+indusudheesh@users.noreply.github.com>
2022-02-10 16:22:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,31 @@ const cors=require('cors')
 const fileUpload=require('express-fileupload')
 const PORT=process.env.port||8080
 
+app.use(express.static(path.resolve(__dirname, "./build")));
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader("Access-Control-Allow-Origin", "*");
+  
+    // Request methods you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,POST,OPTIONS,PUT,PATCH,DELETE"
+    );
+  
+    // Request headers you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-with,content-type"
+    );
+  
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", true);
+  
+    // Pass to next layer of middleware
+    next();
+  });
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use('/public',express.static('public'))
@@ -23,7 +48,7 @@ app.use('/api/article',articleRouter)
 
 
 connection()
-app.use(express.static(path.join(__dirname,'./build')))
+
 app.get('*',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./build','index.html'))
 })
